4e8ca6081aa9b89431d8365c32576e9b7e73f8d3
Yossefelhamoley <138775003+Yossefelhamoley@users.noreply.github.com>
2023-07-11 06:39:20 +0300

    Add files via upload

---
+++ bonus_task1.ino
@@ -0,0 +1,123 @@
+// C++ code
+//
+int LED_1 = 13;
+int LED_2 = 12;
+int LED_3 = 11;
+int LED_4 = 10;
+int LED_5 = 9;
+void setup()
+{
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
+  pinMode(LED_5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_1, HIGH);
+  delay(1000); 
+  digitalWrite(LED_1, LOW); 
+  digitalWrite(LED_2, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000); 
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_4, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000); 
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000); 
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_4, LOW);
+  digitalWrite(LED_5, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000); 
+  digitalWrite(LED_1, LOW); 
+  digitalWrite(LED_2, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000); 
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_4, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000); 
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(1000); 
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(1000);
+  digitalWrite(LED_1, HIGH);
+  delay(3000);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_4, LOW);
+  digitalWrite(LED_5, LOW);
+  delay(5000);
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
